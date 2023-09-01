1e7d52aa4cccc9e70b2a704001eaea3cea95326f
Keisuke Takagi <mit@takagi.app>
2023-09-01 23:39:55 +0900

    feat(preset): apply textField recipe

--- packages/components/ui/text-field/text-field.stories.tsx
+++ packages/components/ui/text-field/text-field.stories.tsx
@@ -1,6 +1,7 @@
 import TextField from './text-field';
+import { textField } from '../../styled-system/recipes';
 export function Default() {
   return <TextField />;
 }
-Default.parameters = { chromatic: { disable: false } };
+Default.parameters = { chromatic: { disable: false }, args: { ...textField.raw({}) } };
 export default { title: 'Components/TextField' };
