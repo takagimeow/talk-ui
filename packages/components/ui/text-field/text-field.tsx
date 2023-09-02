f426b8f0be213e6ca449cefcf671910ff562e758
Keisuke Takagi <mit@takagi.app>
2023-09-02 23:48:02 +0900

    feat(ui): add border

--- packages/components/ui/text-field/text-field.tsx
+++ packages/components/ui/text-field/text-field.tsx
@@ -1,7 +1,15 @@
 import { cva } from '../../styled-system/css';
-import { textField } from '../../styled-system/recipes';
+// import { textField } from '../../styled-system/recipes';
 
-// const textField = cva({});
+const textField = cva({
+  base: {
+    width: '100%',
+    borderRadius: '8px',
+    boxShadow: '1.5px 1.5px 8px 8px rgba(0, 0, 0, 0.1)',
+    padding: '16px',
+    border: '1px solid #E5E5E5',
+  },
+});
 
 const TextField = () => {
   return <input aria-label="Text Field" className={textField()} />;
