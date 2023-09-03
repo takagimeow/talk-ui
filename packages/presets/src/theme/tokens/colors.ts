81370231a1947a810a395502659928a30c44769b
Keisuke Takagi <mit@takagi.app>
2023-09-04 02:02:43 +0900

    chore(presets): update color tokens

--- packages/presets/src/theme/tokens/colors.ts
+++ packages/presets/src/theme/tokens/colors.ts
@@ -4,4 +4,121 @@ export const colors = defineTokens.colors({
   primary: {
     value: '#FFFF00',
   },
+  polar: {
+    value: '#F7F7F',
+  },
+  swan: {
+    value: 'E5E5E5',
+  },
+  hare: {
+    value: '#AFAFAF',
+  },
+  wolf: {
+    value: '#777777',
+  },
+  eel: {
+    value: '#4B4B4B',
+  },
+  squid: {
+    value: 'EBE3E3',
+  },
+  walkingFish: {
+    value: '#FFDFE0',
+  },
+  flamingo: {
+    value: '#FFB2B2',
+  },
+  pig: {
+    value: '#F5A4A4',
+  },
+  crab: {
+    value: '#FF7878',
+  },
+  cardinal: {
+    value: '#FF4B4B',
+  },
+  fireAnt: {
+    value: '#EA2B2B',
+  },
+  canary: {
+    value: '#FBE56D',
+  },
+  duck: {
+    value: '#FBE56D',
+  },
+  bee: {
+    value: '#FFC800',
+  },
+  lion: {
+    value: '#FFB100',
+  },
+  fox: {
+    value: '#FF9600',
+  },
+  cheetah: {
+    value: '#FFCE8E',
+  },
+  monkey: {
+    value: '#E5A259',
+  },
+  camel: {
+    value: '#E7A601',
+  },
+  guineaPig: {
+    value: '#CD7900',
+  },
+  grizzly: {
+    value: '#A56644',
+  },
+  seaSponge: {
+    value: '#D7FFB8',
+  },
+  turtle: {
+    value: '#A5ED6E',
+  },
+  owl: {
+    value: '#58CC02',
+  },
+  treeFrog: {
+    value: '#58A700',
+  },
+  iguana: {
+    value: '#DDF4FF',
+  },
+  anchovy: {
+    value: '#D2E4E8',
+  },
+  beluga: {
+    value: '#BBF2FF',
+  },
+  moonJelly: {
+    value: '#7AF0F2',
+  },
+  blueJay: {
+    value: '#84D8FF',
+  },
+  macaw: {
+    value: '#1CB0F6',
+  },
+  whale: {
+    value: '#1899D6',
+  },
+  humpback: {
+    value: '#2B70C9',
+  },
+  narwhal: {
+    value: '#1453A3',
+  },
+  starfish: {
+    value: '#FFAADE',
+  },
+  beetle: {
+    value: 'CE82FF',
+  },
+  betta: {
+    value: '#9069CD',
+  },
+  butterfly: {
+    value: '#6F4EA1',
+  },
 });
